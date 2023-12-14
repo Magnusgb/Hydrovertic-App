d83759d6511ad5fea585e3e5c28cf78623797457
Magnus Bomholt <magbom123@gmail.com>
2023-12-14 19:39:29 +0100

    Valideret samt oprydning

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,37 +2,32 @@ var button = document.getElementById("button-click");
 var filterBox = document.getElementById("filter-seachbox");
 var saveButton = document.getElementById("save-button");
 
-// Array med plantenavne
 var planteNavne = ["Basilikum", "Mynte", "Persille"];
 
-// Event til knappen, der håndterer visning/skjul af filter-boksen
+// Event til ikon der viser/skjuler filterboksen
 button.addEventListener("click", function () {
-  if (filterBox.style.display === "none" || filterBox.style.display === "") {
+  if (filterBox.style.display === "none") {
     filterBox.style.display = "block";
   } else {
     filterBox.style.display = "none";
   }
 });
 
-// Event til gem-knappen
+//Funktion & loop der tjekker hvad som er trykket på i filterboks 
 saveButton.addEventListener("click", function () {
 
-  // Loop gennem plantenavne og håndter valgte planter
   for (var i = 0; i < planteNavne.length; i++) {
     var planteNavn = planteNavne[i];
     var checkbox = document.querySelector('input[name="' + planteNavn + '"]');
 
-    // Laver loop igennem igennem array, og tjekker hvad der er trykket på
     if (checkbox.checked) {
       if (planteNavn === "Basilikum") {
         window.location.href = 'basilikum.html';
         return;
       } else {
         alert(planteNavn + " er ikke tilgængelig");
-        return; 
       }
     }
   }
-  alert("Vælg mindst en plante.");
 });
 
